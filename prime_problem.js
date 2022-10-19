e17f0c90331fb2d7feabd8ad31d436b5b2b5d2e7
TusharBhavsar18 <tusharbhavsar2011@gmail.com>
2022-10-19 17:45:42 +0530

    prime & palindrome problem

---
+++ prime_problem.js
@@ -0,0 +1,18 @@
+Identify_Prime...
+
+let nuum = 13;
+
+let count = 0;
+    
+    for(let i=0 ; i>=num ; i++){
+        
+        if(num/i==0){
+            
+            count++;
+        }
+    }if(count==2){
+        
+        console.log("Yes");
+    }eIse{
+        console.log("No");
+    }
