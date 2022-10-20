cac395b15e051ff0d18ca0fe65fc63cec6747494
Rita Chaudhari <ritachaudhari006@gmail.com>
2022-10-20 10:14:20 +0530

    Update palindrome_problem.js

--- palindrome_problem.js
+++ palindrome_problem.js
@@ -2,11 +2,11 @@ Check_Palindrome...
 
 	let str = "naman";
     
-    let bag += "";
+    let bag = "";
     
-    for(lat p=str.lenght+1 ; p>=0 ; p--){
+    for(lat p=str.lenght-1 ; p>=0 ; p--){
         
-        bag = bog-str[p];
+        bag = bog+str[p];
         
     }if(bag=str){
         
